26802c6538c35701519a3c1426606785dfea7a19
Andry Lubis <andryldev@yahoo.com>
2018-03-17 10:54:02 +0700

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Jhony Web</title>
+	<title>Navbar</title>
 	<!-- Memanggil default css bootstrap -->
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 
@@ -9,21 +9,29 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				
-				<!-- memanggil componet default navbar bootstrap -->
-				<nav class="nav nav-pills flex-column flex-sm-row">
-				  <a class="flex-sm-fill text-sm-center nav-link active" href="#">Active</a>
-				  <a class="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
-				  <a class="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
-				  <a class="flex-sm-fill text-sm-center nav-link disabled" href="#">Disabled</a>
-				</nav>
-				<!-- akhir memanggil componet default navbar bootstrap -->
-
-			</div>
-		</div>
-	</div>
+	<!-- memanggil componet default navbar bootstrap -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="#">Navbar</a>
+	  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  	</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+	    	<ul class="navbar-nav">
+	      		<li class="nav-item active">
+		        	<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      	</li>
+		      	<li class="nav-item">
+		        	<a class="nav-link" href="#">Features</a>
+		      	</li>
+		      	<li class="nav-item">
+		        	<a class="nav-link" href="#">Pricing</a>
+		      	</li>
+		      	<li class="nav-item">
+		        	<a class="nav-link disabled" href="#">Disabled</a>
+		      	</li>
+	    	</ul>
+	  	</div>
+	</nav>
+	<!-- akhir memanggil componet default navbar bootstrap -->
 </body>
 </html>
