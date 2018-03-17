8703de8ab8bbbc1fed526cf3f8ec2f9c85677574
Andry Lubis <andryldev@yahoo.com>
2018-03-17 10:11:13 +0700

    upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# custom-navbar-bootstrap-v4
+# Custom Navbar Bootstrap Version 4
 Simple Custom Navbar Bootstrap V.4
+
+## Step by step
+ - Download Bootstrap V 4.0 : https://getbootstrap.com/docs/4.0/getting-started/download/
+ - Extract and save in your folder, example "custom-navbar-bootstrap-v4/assets"
+ - create file index.php in folder "custom-navbar-bootstrap-v4"
+ - type html structur and navbar bootstrap component
+ - create style.css in folder "assets/css/"
+ - create custom style in here file "style.css"
+
+
+Thank You,
+Andry :D
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jhony Web</title>
+	<!-- Memanggil default css bootstrap -->
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+	<!-- memanggil style css custom yang dibuat sendiri -->
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				
+				<!-- memanggil componet default navbar bootstrap -->
+				<nav class="nav nav-pills flex-column flex-sm-row">
+				  <a class="flex-sm-fill text-sm-center nav-link active" href="#">Active</a>
+				  <a class="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
+				  <a class="flex-sm-fill text-sm-center nav-link" href="#">Link</a>
+				  <a class="flex-sm-fill text-sm-center nav-link disabled" href="#">Disabled</a>
+				</nav>
+				<!-- akhir memanggil componet default navbar bootstrap -->
+
+			</div>
+		</div>
+	</div>
+</body>
+</html>
